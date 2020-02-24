e45723cf2cad36ddddc46c817d6bf06ad471f2f9
Emilia <emilia.mena26@gmail.com>
2020-02-24 18:19:32 -0500

    Done.

--- src/MouseComponent.js
+++ src/MouseComponent.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 
+
 export default class MouseComponent extends Component {
 
-  componentDidMount() {
-    require("./hoverEffect.js")
-  }
-  
-  render() {
-    return (
-      <div className="bar" id="mouse">
-        <canvas id="canvas"></canvas>
-      </div>
-    )
-  }
+    componentDidMount() {
+        require("./hoverEffect.js")
+    }
+
+    render() {
+        return ( <
+            div className = "bar"
+            id = "mouse" >
+            <
+            canvas id = "canvas" > < /canvas> < /
+            div >
+        )
+    }
 }
